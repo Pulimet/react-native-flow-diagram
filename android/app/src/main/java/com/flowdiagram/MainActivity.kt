f2334e084fbac51383faf347017f54a5c0ce4175
Korolev, Alexey (ak3140) <ak3140@att.com>
2025-09-04 22:00:16 +0300

    MainActivity.kt - Make network call

--- android/app/src/main/java/com/flowdiagram/MainActivity.kt
+++ android/app/src/main/java/com/flowdiagram/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.flowdiagram
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 import net.alexandroid.flowdiagram.LogTime
 import net.alexandroid.flowdiagram.LogLevel
-import android.os.Bundle
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
 
 class MainActivity : ReactActivity() {
 
@@ -20,5 +26,23 @@ class MainActivity : ReactActivity() {
         LogTime.onIntent(intent)
         LogTime.logSync("MainActivity.onCreate")
         LogTime.logAsync("MainActivity.onCreate -> Async Example", logLevel = LogLevel.INFO)
+        makeNetworkRequest()
+    }
+
+    private fun makeNetworkRequest() {
+        val request = Request.Builder()
+            .url("https://api.agify.io/?name=Alexey")
+            .build()
+        val okHttpClient = OkHttpClient.Builder().build()
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                LogTime.logAsync("Network call failed: ${e.message}", logLevel = LogLevel.DEBUG)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                LogTime.logAsync("Network call successful: ${response.code}", logLevel = LogLevel.INFO)
+                response.body?.close()
+            }
+        })
     }
 }
